5479561bdeaf837a9b64266b4ea7d9e189990bde
Andrew Cambridge <56008700+ac3343@users.noreply.github.com>
2019-10-12 15:39:16 -0400

    Added player attack logic wihtout hitboxes

---
+++ QwaxGame/Assets/Scripts/Attack.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public enum AttackStates
+    {
+        WindUp,
+        Attack,
+        CoolDown
+    }
+    //Fields
+    public int windUpFrames;
+    public int attackFrames;
+    public int coolDownFrames;
+    public AttackStates currentAttackState;
+    int totalFrames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalFrames = windUpFrames + attackFrames + coolDownFrames;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool AttackFinished(int currentFrame)
+    {
+        if(currentFrame > totalFrames)
+        {
+            return true;
+        }
+        else
+        {
+            if(currentFrame < windUpFrames)
+            {
+                currentAttackState = AttackStates.WindUp;
+            }
+            else if(currentFrame < windUpFrames + attackFrames)
+            {
+                currentAttackState = AttackStates.Attack;
+            }
+            else if(currentFrame < totalFrames)
+            {
+                currentAttackState = AttackStates.CoolDown;
+            }
+
+            return false;
+        }
+    }
+
+    public void GiveFrames(int windUp, int attack, int coolDown)
+    {
+        windUpFrames = windUp;
+        attackFrames = attack;
+        coolDownFrames = coolDown;
+    }
+}
--- QwaxGame/Assets/Scripts/Player.cs
+++ QwaxGame/Assets/Scripts/Player.cs
@@ -14,12 +14,20 @@ public class Player : MonoBehaviour
     //Fields
     public Vector3 position;
     public float speed;
+    public KeyCode attackButton;
     PlayerStates currentState = PlayerStates.Standing;
+    Attack[] swings;
+    int currentSwing;
+    int currentAttackFrame;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         position = new Vector3(0, 0, 0);
+        swings = new Attack[3];
+        currentSwing = 0;
+        currentAttackFrame = 0;
     }
 
     // Update is called once per frame
@@ -34,7 +42,26 @@ public class Player : MonoBehaviour
 
         switch (currentState)
         {
+            case PlayerStates.Standing:
+                break;
+            case PlayerStates.Walking:
+                break;
+            case PlayerStates.Attacking:
+                if (swings[currentSwing].AttackFinished(currentAttackFrame))
+                {
+                    currentAttackFrame = 0;
+                    currentSwing = 0;
+                }
+                else if(swings[currentSwing].currentAttackState == Attack.AttackStates.CoolDown && Input.GetKeyDown(attackButton) && currentSwing < 2)
+                {
+                    currentSwing++;
+                    currentAttackFrame = 0;
+                }
+                else if(swings[currentSwing].currentAttackState == Attack.AttackStates.Attack)
+                {
 
+                }
+                break;
         }
         if (Input.GetKey(KeyCode.A))
         {
