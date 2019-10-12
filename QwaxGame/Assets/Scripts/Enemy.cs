e09b3324e3d8dc2e30731c3d599a08a5010cc2d4
Joshua Radford <joshuaradford99@yahoo.com>
2019-10-12 15:49:41 -0400

    Added to Enemy and created EnemyManager

--- QwaxGame/Assets/Scripts/Enemy.cs
+++ QwaxGame/Assets/Scripts/Enemy.cs
@@ -12,19 +12,46 @@ public class Enemy : MonoBehaviour
         Attacking,
     }
 
+    public Player player;
     public Vector3 position;
     public float speed;
-    EnemyStates currentState = EnemyStates.Standing;
+    EnemyStates currentState;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentState = EnemyStates.Walking;
         position = new Vector3(5, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(currentState)
+        {
+            case EnemyStates.Standing:
+                break;
+            case EnemyStates.Walking:
+                WalkEnemy();
+                break;
+            case EnemyStates.Attacking:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void WalkEnemy()
+    {
+        position = transform.position;
+
+        position += Vector3.Normalize(player.position - position) * speed;
+
+        transform.position = position;
+    }
+
+    void AttackEnemy()
+    {
+
     }
 }
