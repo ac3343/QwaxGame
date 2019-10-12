cfb563510c25a4bc9f4d433114a52c68018e4cca
Joshua Radford <joshuaradford99@yahoo.com>
2019-10-12 14:46:13 -0400

    Enemy.cs created

---
+++ QwaxGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    enum EnemyStates
+    {
+        Standing,
+        Walking,
+        Attacking,
+    }
+
+    public Vector3 position;
+    public float speed;
+    EnemyStates currentState = EnemyStates.Standing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = new Vector3(5, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
