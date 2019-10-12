c17a23aff3123fbe8a1c99a284d5eaea15ec506d
Andrew Cambridge <56008700+ac3343@users.noreply.github.com>
2019-10-12 13:06:49 -0400

    Started player script

---
+++ QwaxGame/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    enum PlayerStates
+    {
+        Standing,
+        Walking, 
+        Attacking
+    }
+
+    //Fields
+    public float speed;
+    PlayerStates currentState = PlayerStates.Standing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InputUpdate()
+    {
+        Vector3 position = transform.position;
+
+        switch (currentState)
+        {
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            position.x -= speed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            position.x -= speed;
+        }
+
+        position = transform.position;
+    }
+}
